401566cd65c3f27855bfde4215c6291f8cef9586
Jose luis Perez rabasot <jperez-r@c1r1s5.42madrid.com>
2024-04-26 21:13:07 +0200

    movement_counter

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: jperez-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 18:30:59 by jperez-r          #+#    #+#             */
-/*   Updated: 2024/04/23 19:44:25 by jperez-r         ###   ########.fr       */
+/*   Updated: 2024/04/26 18:39:09 by jperez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,11 +32,28 @@
 # define A 0//97
 # define S 1//115
 # define D 2//100
+# define Z 6
+# define Q 12
+# define UP 62
+# define LEFT 59
+# define DOWN 61
+# define RIGHT 60
 # define ESC 53 //65307
+
+# define WHITE 0x00FFFFFF
+# define BLACK 0x00000000
+# define RED 0x00FF0000
+# define GREEN 0x00FFFFFF
+# define BLUE 0x0000FF00
 
 typedef struct	s_vars {
 	void	*mlx;
 	void	*win;
+	int	x;
+	int	xlimit;
+	int	y;
+	int	ylimit;
+	int	moves;
 }				t_vars;
 
 int		error_so_long(int er, char *s);
